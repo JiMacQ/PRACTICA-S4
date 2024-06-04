fa09d0d50cca164c83711983f9a10e64b446c2b3
JennyferPallo <jpallop@uteq.edu.ec>
2024-06-04 10:41:48 -0500

    JMQ-JPP-BRL

--- app/src/main/java/com/example/myapplistausuarios_it2024/MainActivityListaUsuario.java
+++ app/src/main/java/com/example/myapplistausuarios_it2024/MainActivityListaUsuario.java
@@ -27,7 +27,7 @@ public class MainActivityListaUsuario extends AppCompatActivity
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_lista_usuario);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.txtregistro), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
